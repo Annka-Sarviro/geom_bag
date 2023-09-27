ad9368ad1509fff8e6bb673439ea4fb0ca5963ac
Ann Sarviro <Annka_nuha@i.ua>
2023-09-27 23:46:30 +0300

    fix:search product function

--- src/components/groupMenu/GroupMenuCard/GroupMenuCard.tsx
+++ src/components/groupMenu/GroupMenuCard/GroupMenuCard.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useGroupContext } from '@/app/context';
+import { useFilterContext, useGroupContext } from '@/app/context';
 import Paragraph from '@/components/typography/Paragraph';
 import Link from 'next/link';
 import { Image as DatoImage } from 'react-datocms';
@@ -8,8 +8,10 @@ import { Image as DatoImage } from 'react-datocms';
 const GroupMenuCard = ({ item, title, id }: any) => {
   const data = item.responsiveImage.src;
   const { groupFilter, setGroupFilter } = useGroupContext();
+  const { searchfilter, setSearchfilter } = useFilterContext();
 
   const handleClick = () => {
+    setSearchfilter('');
     return setGroupFilter(id);
   };
 
